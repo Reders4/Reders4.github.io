f30db738efd5c6de442efecb104d8cddd0ba77fb
Никита Бойченцев <4reders4@gmail.com>
2024-01-29 21:58:59 +0300

    homework1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -11,8 +11,14 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
- *
- * Продукт (Product) содержит
+ */
+type Category = {
+    id: string,
+    name: string,
+    photo?: string
+}
+
+ /* Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
  * - photo (строка)
@@ -21,8 +27,20 @@
  * - oldPrice (число, необязательно)
  * - price (число)
  * - category (Категория)
- *
- * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
+ */
+
+ type Product = {
+    id: string,
+    name: string,
+    photo: string,
+    desc?: string,
+    createdAt: string,
+    oldPrice?: number,
+    price: number,
+    category: Category
+ }
+
+ /* Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
  *
  * Трата (Cost) содержит
  * - id (строка)
@@ -43,14 +61,79 @@
  * - type ('Profit')
  * */
 
+type Cost = {
+    id: string,
+    name: string,
+    desc?: string,
+    createdAt: string,
+    amount: number,
+    category: Category,
+    type: 'Cost'
+}
+
+type Profit = {
+    id: string,
+    name: string,
+    desc?: string,
+    createdAt: string,
+    amount: number,
+    category: Category,
+    type: 'Profit'
+}
+
+type Operation = Cost | Profit;
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+ export const createRandomProduct = (createdAt: string): Product => {
+    const timestamp = new Date(createdAt).getTime();
+    const randomNum = Math.floor(Math.random() * 1000000); 
+
+    const productId = `${timestamp}-${randomNum}`;
+    const categoryId = `${randomNum}-${timestamp}`;
+
+    let price = Math.random();
+    if(price < 0){
+        price *= -1;
+    }
+
+    return {
+        id: productId,
+        name: 'Product_' + productId,
+        photo: 'Photo_' + timestamp,
+        createdAt: createdAt,
+        price: price,
+        category: {
+            id: categoryId,
+            name: 'Category_' + categoryId
+        }
+    }
+ };
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+ export const createRandomOperation = (createdAt: string): Operation => {
+    const timestamp = new Date(createdAt).getTime();
+    const randomNum = Math.floor(Math.random() * 1000000); 
+
+    const operationId = `${timestamp}-${randomNum}`;
+    const categoryId = `${randomNum}-${timestamp}`;
+
+    const amount = Math.random();
+
+    return {
+        id: operationId,
+        name: 'Operation_' + operationId,
+        createdAt: createdAt,
+        amount: amount,
+        category: {
+            id: categoryId,
+            name: 'Category_' + categoryId
+        },
+        type: amount < 0 ? 'Cost' : 'Profit'
+    }
+ };
